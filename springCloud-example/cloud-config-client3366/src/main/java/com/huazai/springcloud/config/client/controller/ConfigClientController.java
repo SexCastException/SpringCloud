9e7dab30954d576440d5af573adb9e96722a6c25
pyh <739967221@qq,cin>
2020-08-19 23:37:34 +0800

    springcloud bus 消息总线和rabbitMQ配置

---
+++ springCloud-example/cloud-config-client3366/src/main/java/com/huazai/springcloud/config/client/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.huazai.springcloud.config.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pyh
+ * @date 2020/8/19 21:44
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "configInfo：" + configInfo + "\t\n\n configInfo：" + configInfo;
+    }
+}
