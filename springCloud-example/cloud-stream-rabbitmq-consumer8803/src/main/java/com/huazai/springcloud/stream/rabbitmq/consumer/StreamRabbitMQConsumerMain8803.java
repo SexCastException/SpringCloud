4da03226b4b7411d96dfe07fc54dc1a9ff87efae
pyh <739967221@qq,cin>
2020-08-25 22:28:09 +0800

    springCloud stream 分组

---
+++ springCloud-example/cloud-stream-rabbitmq-consumer8803/src/main/java/com/huazai/springcloud/stream/rabbitmq/consumer/StreamRabbitMQConsumerMain8803.java
@@ -0,0 +1,15 @@
+package com.huazai.springcloud.stream.rabbitmq.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pyh
+ * @date 2020/8/24 20:29
+ */
+@SpringBootApplication
+public class StreamRabbitMQConsumerMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamRabbitMQConsumerMain8803.class, args);
+    }
+}
