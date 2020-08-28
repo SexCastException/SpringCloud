ab1c0a5e44113d051ea1ea1f9892101ceef310c7
pyh <739967221@qq,cin>
2020-08-28 19:43:01 +0800

    add nacos test modules

---
+++ springCloud-example/cloud-alibaba-provider-payment9001/src/main/java/com/huazai/springcloud/alibaba/provider/payment/AlibabaProviderPaymentMain9001.java
@@ -0,0 +1,17 @@
+package com.huazai.springcloud.alibaba.provider.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author pyh
+ * @date 2020/8/27 22:05
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AlibabaProviderPaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaProviderPaymentMain9001.class, args);
+    }
+}
