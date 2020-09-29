bd60d9ef7412164373f76ab5a61d86e469c6f6b5
pyh <739967221@qq,cin>
2020-09-29 21:59:29 +0800

    修改模块名称以及相关配置yml配置文件

--- springCloud-example/cloud-alibaba-seata-account-service2003/src/main/java/com/huazai/springcloud/alibaba/seata/order/service/SeataOrderMain2003.java
+++ springCloud-example/cloud-alibaba-seata-account-service2003/src/main/java/com/huazai/springcloud/alibaba/seata/order/service/SeataOrderMain2003.java
@@ -2,6 +2,7 @@ package com.huazai.springcloud.alibaba.seata.order.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -9,9 +10,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @author pyh
  * @date 2020/9/22 22:49
  */
-@SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//取消数据源自动创建的配置
 public class SeataOrderMain2003 {
     public static void main(String[] args) {
         SpringApplication.run(SeataOrderMain2003.class, args);
